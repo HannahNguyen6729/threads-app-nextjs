25397e6369ed7f01ef5e2af91fcfc6086db3e1e5
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-11-16 15:56:38 +0200

    get user by id to render posts

--- lib/serverActions/user.action.ts
+++ lib/serverActions/user.action.ts
@@ -49,3 +49,25 @@ export const getUser = async (userId: string): Promise<any> => {
     throw new Error(`failed to get user: ${error.message}`);
   }
 };
+
+export const getUserPosts = async (userId: string) => {
+  try {
+    connectDatabase();
+
+    const user = await UserModel.findOne({ id: userId }).populate({
+      path: 'threads',
+      populate: [
+        {
+          path: 'children',
+          populate: { path: 'author', select: 'name image id' },
+        },
+        //community
+        // { path: 'community', select: 'name id image _id' },
+      ],
+    });
+
+    return user;
+  } catch (error: any) {
+    throw new Error(`failed to get user: ${error.message}`);
+  }
+};
