7c2a2f08453337e31b26db89f465bd05e337f662
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-11-17 10:34:38 +0200

    add server action: getUsers

--- lib/serverActions/user.action.ts
+++ lib/serverActions/user.action.ts
@@ -4,6 +4,7 @@
 import { revalidatePath } from 'next/cache';
 import { UserModel } from '../models/user.model';
 import { connectDatabase } from '../mongoose';
+import { FilterQuery, SortOrder } from 'mongoose';
 
 interface UserProps {
   userId: string;
@@ -69,5 +70,55 @@ export const getUserPosts = async (userId: string) => {
     return user;
   } catch (error: any) {
     throw new Error(`failed to get user: ${error.message}`);
+  }
+};
+
+export const getUsers = async ({
+  userId,
+  searchString = '',
+  pageSize = 20,
+  pageNumber = 1,
+  sortBy = 'desc',
+}: {
+  userId: string;
+  searchString?: string;
+  pageSize?: number;
+  pageNumber?: number;
+  sortBy?: SortOrder;
+}) => {
+  try {
+    connectDatabase();
+
+    //count the number of users to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    //create case-insensitive search string
+    const regex = new RegExp(searchString, 'i');
+
+    //create a query obj to filter users those match search criteria
+    const query: FilterQuery<typeof UserModel> = { id: { $nq: userId } }; //remove the current user from the user list
+
+    if (searchString.trim() !== '') {
+      //if search string is not empty, add $or operator to match username/name
+      query.$or = [
+        { username: { $regex: regex } },
+        { password: { $regex: regex } },
+      ];
+    }
+
+    const users = await UserModel.find(query)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .sort({ createdAt: sortBy });
+
+    //count the total number of users that match search criteria
+    const totalUsersCount = await UserModel.countDocuments(query);
+
+    //check if there are more users than the number of users on the current page
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`failed to get users: ${error.message}`);
   }
 };
