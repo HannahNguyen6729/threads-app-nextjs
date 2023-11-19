d5f6a2d85a0d4eecc0496ee8e5eac4bc5ba271a3
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-11-19 20:47:47 +0200

    render comments with conditions

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -76,7 +76,7 @@ const ThreadCard = ({
                 <Link href={`/thread/${id}`}>
                   <Image
                     src="/assets/reply.svg"
-                    alt="heart"
+                    alt="reply"
                     width={24}
                     height={24}
                     className="cursor-pointer object-contain"
@@ -111,6 +111,28 @@ const ThreadCard = ({
         {/*    TODO: Delete threads */}
         {/*    TODO: show comment logos */}
       </div>
+
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && '-ml-5'} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? 'ies' : 'y'}
+            </p>
+          </Link>
+        </div>
+      )}
+
       {!isComment && community && (
         <Link
           href={`/communities/${community.id}`}
