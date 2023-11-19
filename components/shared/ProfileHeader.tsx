1fb9b06f6973c3bcb9aced71eddc27a281f5599e
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-11-19 21:32:23 +0200

    update ProfileHeader component to show communities

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Props {
   accountId: string;
@@ -41,6 +42,20 @@ export default function ProfileHeader({
             </div>
           </div>
           {/*   TODO: render communities */}
+          {accountId === authUserId && type !== 'Community' && (
+            <Link href="/profile/edit">
+              <div className="flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2">
+                <Image
+                  src="/assets/edit.svg"
+                  alt="logout"
+                  width={16}
+                  height={16}
+                />
+
+                <p className="text-light-2 max-sm:hidden">Edit</p>
+              </div>
+            </Link>
+          )}
         </div>
       </div>
       <p className="mt-6 max-w-lg text-light-2"> {bio} </p>
